4ef4fd18122f670957ccf798ff336e1440d943ee
yashG <yrg.creation.ai@gmail.com>
2025-08-13 01:41:54 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+document.getElementById("search-icon").addEventListener("click", function (event) {
+    let searchBar = document.getElementById("searchbar-box");
+
+    // Show the search bar
+    searchBar.style.display = "inline-block";  
+    searchBar.focus();
+
+    // Stop the click from bubbling up to the document
+    event.stopPropagation();
+});
+
+// Listen for clicks on the whole document
+document.addEventListener("click", function (event) {
+    let searchContainer = document.getElementById("searchbar-box");
+    let bodyblurr = document.getElementById("main");
+
+    // Check if the click was OUTSIDE the search container
+    if (!searchContainer.contains(event.target)) {
+        document.getElementById("searchbar-box").style.display = "none"; // Hide search bar
+    }
+});
+
+window.addEventListener("scroll", function () {
+    let header = document.querySelector("header");
+
+    if (window.scrollY > 5) {  // When scrolled more than 50px
+        header.classList.add("header-scrolled");
+    } else {
+        header.classList.remove("header-scrolled");
+    }
+});
+ 
+
